2d1cf300706ccb48eee9c04af64e956d3c71cd63
Emanuel Vogt <contatoemanuel3@gmail.com>
2025-01-20 18:57:27 -0300

    feat(header): implement header

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import { LuAlignJustify } from "react-icons/lu";
 import { Button } from "../components/Button";
+import { RiVideoUploadLine } from "react-icons/ri";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CiUser } from "react-icons/ci";
+import { LuMic } from "react-icons/lu";
+import { LuSearch } from "react-icons/lu";
+import { useState } from "react";
+import { LuArrowLeft } from "react-icons/lu";
 
 export default function Header() {
+  const [fullState, setFullState] = useState(false);
+
+  const handleOpenSearch = () => {
+    setFullState(!fullState);
+  };
+
   return (
     <div className="flex justify-between lg:gap-20 pt-2 mb-6 mx-4">
-      <div className="flex gap-4 items-center flex-shrink-0">
+      <div
+        className={`gap-4 items-center flex-shrink-0 ${
+          fullState ? "hidden lg:flex" : "flex"
+        }`}
+      >
         <Button size="icon" variant="default">
           <LuAlignJustify />
         </Button>
@@ -13,8 +32,61 @@ export default function Header() {
           <Image src="/images/youtube.png" width={48} height={48} alt="logo" />
         </a>
       </div>
+      <form
+        className={`gap-4 flex-grow justify-center align-middle ml-4 ${
+          fullState ? "flex" : "hidden md:flex"
+        }`}
+      >
+        {fullState && (
+          <Button
+            className="md:hidden"
+            onClick={handleOpenSearch}
+            type="button"
+            variant="ghost"
+            size="icon"
+          >
+            <LuArrowLeft />
+          </Button>
+        )}
 
-      <p>aaa</p>
+        <div className="flex flex-grow max-w-[600]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+          />
+          <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0 bg-secondary-default">
+            <LuSearch />
+          </Button>
+        </div>
+        <Button
+          type="button"
+          variant="default"
+          size="icon"
+          className="flex-shrink-0"
+        >
+          <LuMic />
+        </Button>
+      </form>
+      <div className={`flex-shrink-0 md:gap ${fullState ? "hidden lg:flex" : "flex"}`}>
+        <Button
+          size="icon"
+          variant="default"
+          className="md:hidden"
+          onClick={handleOpenSearch}
+        >
+          <LuSearch />
+        </Button>
+        <Button size="icon" variant="default">
+          <RiVideoUploadLine />
+        </Button>
+        <Button size="icon" variant="default">
+          <IoIosNotificationsOutline />
+        </Button>
+        <Button size="icon" variant="default">
+          <CiUser />
+        </Button>
+      </div>
     </div>
   );
 }
