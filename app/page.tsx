60c2da8ad7280659afa6aaf43d2eef23df51d625
Emanuel Vogt <contatoemanuel3@gmail.com>
2025-01-21 14:52:19 -0300

    feat(home): implement categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import Header from "./ui/Header";
+import Categories from "./ui/Categories";
+import { categories } from "./fake-data";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
+  const handleSelectCategory = (category: string) => {
+    setSelectedCategory(category);
+  };
+
   return (
-    <Header />
-  )
+    <div className="max-h-screen flex flex-col">
+      <Header />
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <Categories
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={handleSelectCategory}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
